2860d35138379750d4780feb7ee83c91f0e3888f
Shubhra02 <Shubhra.sharma1007@gmail.com>
2017-03-31 17:24:18 +0530

    implemented unit test cases for SolrAccess

--- src/test/scala/com/knoldus/solrService/SolrAccessSpec.scala
+++ src/test/scala/com/knoldus/solrService/SolrAccessSpec.scala
@@ -1,25 +1,43 @@
-/*
 package com.knoldus.solrService
 
-import java.io.File
-
-import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer
-import org.apache.solr.core.CoreContainer
-import org.scalatest.{BeforeAndAfterAll, FunSuite}
+import com.knoldus.solrService.factories.{BookDetails, SolrAccess, SolrClientAccess}
+import org.junit.runner.RunWith
+import org.mockito.Mockito._
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
 import org.scalatest.mock.MockitoSugar
 
-class SolrAccessSpec extends FunSuite with MockitoSugar with BeforeAndAfterAll {
+@RunWith(classOf[JUnitRunner])
+class SolrAccessSpec extends FunSuite with MockitoSugar {
 
-  var server: EmbeddedSolrServer = null
+  private val solrAccessClient = mock[SolrClientAccess]
+  private val solrAccess = new SolrAccess(solrAccessClient)
+  val book = BookDetails("12", Array("abc"), "jhvh", "ghvghv", Some("avc"), 1, "dfg", true, 1.3, 22)
 
-  override def beforeAll() {
-    val container = new CoreContainer("testConf")
-    container.load()
-    server = new EmbeddedSolrServer(container, "collection1")
+  test("to create or update record if already created") {
+    when(solrAccessClient.insertRecord(book)).thenReturn(Some(1))
+    val result: Option[Int] = solrAccess.createOrUpdateRecord(book)
+    assert(result == Some(1))
   }
 
-  override def afterAll(): Unit = {
-    server.close()
+  test("to  find all record") {
+    when(solrAccessClient.fetchData("*:*")).thenReturn(Some(List(book)))
+    val result: Option[List[BookDetails]] = solrAccess.findAllRecord
+    assert(result == Some(List(book)))
   }
+
+  test("to  find record with keyword") {
+    when(solrAccessClient.fetchData(" ")).thenReturn(Some(List(book)))
+    val result: Option[List[BookDetails]] = solrAccess.findRecordWithKeyword(" ")
+    assert(result == Some(List(book)))
+  }
+
+  test("to  find record with key and value") {
+    when(solrAccessClient.fetchData("id:12")).thenReturn(Some(List(book)))
+    val result: Option[List[BookDetails]] = solrAccess.findRecordWithKeyAndValue("id", "12")
+    assert(result == Some(List(book)))
+  }
+
+
+
 }
-*/
